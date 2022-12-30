12490dc8a459f3243f25870f63060e9fc959deef
AndersonLoan <97067748+AndersonLoan@users.noreply.github.com>
2022-12-29 22:43:05 -0600

    Add files via upload

---
+++ following_directions.py
@@ -0,0 +1,21 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: Anderson Wayne Loan
+# Section: 574
+# Assignment: Lab 1.12 Following Directions
+# Date: 08/28/2020
+#
+
+print("This shows the evaluation of tan(x)/x evaluated close to x=0")
+print("My guess is 2")
+x= 2 
+value = 0
+while x<8: 
+    value = ((x**2) - 1) / (x-1)
+    print(value)
+    x = x + 1 
+print("")
+print("My guess was off by a lot.")
+    
